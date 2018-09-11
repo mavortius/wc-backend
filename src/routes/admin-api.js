4370018ef0a40bae4724fcdb621ed96afc327dc0
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-09-11 17:12:37 -0300

    Created update score endpoint

--- src/routes/admin-api.js
+++ src/routes/admin-api.js
@@ -2,6 +2,28 @@ const express = require('express');
 const api = express.Router();
 const Match = require('../models/match');
 
+const updateScore = async (matchId, teamId) => {
+    try {
+        let match = await Match.findById(matchId);
+
+        if (match == null) {
+            throw new Error('Match not found');
+        }
+
+        if (teamId === 'team_id') {
+            match.score.team_1++;
+        } else {
+            match.score.team_2++;
+        }
+
+        match = await match.save();
+
+        return match;
+    } catch (err) {
+        throw err;
+    }
+};
+
 api
     .route('/admin/match/:id?')
     .post((req, res, next) => {
@@ -11,7 +33,12 @@ api
             .catch(err => next(err));
     })
     .put((req, res, next) => {
+        const matchId = req.params.id;
+        const teamId = req.body.teamId;
 
+        updateScore(matchId, teamId)
+            .then(match => res.json(match))
+            .catch(err => next(err));
     });
 
 module.exports = api;
